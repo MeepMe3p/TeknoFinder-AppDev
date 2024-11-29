35490eee5d5ad02cb762c77de80fd2a8ee7fe5b9
Jake <clarin.jake26@gmail.com>
2024-11-29 14:53:55 +0800

    (Done) Added finished progress for the main page - home. Ready to be converted to React.js

--- main-page/js/main-page.js
+++ main-page/js/main-page.js
@@ -2,6 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 <<<<<<< HEAD
+<<<<<<< HEAD
 	const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
 	window.addEventListener("scroll", () => {
@@ -100,45 +101,110 @@ thumbnails.forEach((thumbnail, index) => {
 })
 =======
   const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+=======
+	const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+>>>>>>> 29cb6ab ((Done) Added finished progress for the main page - home. Ready to be converted to React.js)
 
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-      scrollToTopBtn.style.display = "block";
-    } else {
-      scrollToTopBtn.style.display = "none";
-    }
-  });
+	window.addEventListener("scroll", () => {
+		if (window.scrollY > 300) {
+		  scrollToTopBtn.style.display = "block";
+		} else {
+		  scrollToTopBtn.style.display = "none";
+		}
+	});
 
-  scrollToTopBtn.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  });
+	scrollToTopBtn.addEventListener("click", () => {
+		window.scrollTo({
+		  top: 0,
+		  behavior: "smooth",
+		});
+	});
 });
 
 
-const howItWorksLink = document.getElementById("howItWorksLink");
-const howItWorksSection = document.getElementById("howItWorks");
+const howItWorksLink = document.getElementById("citu360Link");
+const howItWorksSection = document.getElementById("citu360");
 
 howItWorksLink.addEventListener("click", function (event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  howItWorksSection.scrollIntoView({
-    behavior: "smooth", 
-    block: "start" 
-  });
+	howItWorksSection.scrollIntoView({
+		behavior: "smooth", 
+		block: "start" 
+	});
 });
 
-const visualMapLink = document.getElementById("visualMapLink");
-const cituMapSection = document.getElementById("cituMap");
+/* Image Slider */
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
 
-visualMapLink.addEventListener("click", function (event) {
-  event.preventDefault();
+// config param
+let countItem = items.length;
+let itemActive = 0;
+// event next click
+next.onclick = function(){
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+//event prev click
+prev.onclick = function(){
+    itemActive = itemActive - 1;
+    if(itemActive < 0){
+        itemActive = countItem - 1;
+    }
+    showSlider();
+}
 
+<<<<<<< HEAD
   cituMapSection.scrollIntoView({
     behavior: "smooth", 
     block: "start"
   });
 });
 >>>>>>> 50d8d15 ((Initial Progress) Added progress for the main page. Yet to be finished.)
+=======
+// auto run slider
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 5000)
+
+function showSlider(){
+    // remove item active old
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    // active new item
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+    setPositionThumbnail();
+
+    // clear auto time run slider
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000)
+}
+
+function setPositionThumbnail () {
+    let thumbnailActive = document.querySelector('.thumbnail .item.active');
+    let rect = thumbnailActive.getBoundingClientRect();
+    if (rect.left < 0 || rect.right > window.innerWidth) {
+        thumbnailActive.scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
+    }
+}
+
+// click thumbnail
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        itemActive = index;
+        showSlider();
+    })
+})
+>>>>>>> 29cb6ab ((Done) Added finished progress for the main page - home. Ready to be converted to React.js)
